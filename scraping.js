deb2aff14b89dad707af28c16cd46e5999267cae
Elijah Meshnick <emeshnick@gmail.com>
2021-01-30 15:54:16 -0500

    first commit

---
+++ scraping.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+async function main() {
+  let totalImg = 0;
+  let imgAlt = 0;
+  let response = await axios.get("https://en.wikipedia.org/wiki/Penguin");
+  let page = response.data;
+  let $ = cheerio.load(page);
+  $("img").each(function () {
+    totalImg++;
+    let image = $(this);
+    console.log(cheerio.html(image));
+    if (image.attr("alt") || image.attr("alt") === "") {
+      imgAlt++;
+    }
+  });
+  console.log(
+    `${imgAlt} out of ${totalImg} total images have alternative descriptions`
+  );
+}
+
+main();
